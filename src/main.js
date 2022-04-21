33bc30a4c7b9b8d5522e893fea0b621db9972aa7
matthewberzinskas <pasomatt@gmail.com>
2022-04-21 12:23:21 -0700

    Routing updates

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
 import App from './App.vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: "/",
+    alias: "/tutorials",
+    name: "tutorials",
+    component: () => import("./components/TutorialsList"),
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("./components/AddTutorial"),
+  },
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+});
+
+Vue.createApp(App).use(router).mount('#app');
