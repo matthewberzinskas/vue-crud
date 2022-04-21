f6a04a710eb81e71e080c7883345ad0cd575ac35
matthewberzinskas <pasomatt@gmail.com>
2022-04-21 12:51:27 -0700

    Updates to tutorial add page

--- src/components/AddTutorial.vue
+++ src/components/AddTutorial.vue
@@ -1,7 +1,34 @@
 <template>
-<div>
-  
-</div>
+  <div class="submit-form">
+    <div v-if="!submitted">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          required
+          v-model="tutorial.title"
+          name="title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input
+          class="form-control"
+          id="description"
+          required
+          v-model="tutorial.description"
+          name="description"
+        />
+      </div>
+      <button @click="saveTutorial" class="btn btn-success">Submit</button>
+    </div>
+    <div v-else>
+      <h4>You submitted successfully!</h4>
+      <button class="btn btn-success" @click="newTutorial">Add</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,26 +40,26 @@ export default {
       tutorial: {
         title: "",
         description: "",
-        published: false
+        published: false,
       },
-      submitted: false
+      submitted: false,
     };
   },
   methods: {
-    saveTutorial () {
+    saveTutorial() {
       var data = {
         title: this.tutorial.title,
         description: this.tutorial.description,
-        published: false
+        published: false,
       };
       TutorialDataService.create(data)
-      .then(() => {
-        console.log("Created new item successfully!");
-        this.submitted = true;
-      })
-      .catch (e => {
-        console.log("Error", e)
-      });
+        .then(() => {
+          console.log("Created new item successfully!");
+          this.submitted = true;
+        })
+        .catch((e) => {
+          console.log("Error", e);
+        });
     },
 
     newTutorial() {
@@ -40,9 +67,16 @@ export default {
       this.tutorial = {
         title: "",
         description: "",
-        published: false
+        published: false,
       };
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style>
+.submit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
