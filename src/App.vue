2d474cf1527460651653df077c2290dbbda30efa
matthewberzinskas <pasomatt@gmail.com>
2022-04-21 12:26:59 -0700

    Main page setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>Tutorial CRUD</div>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">Logo</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/tutorials" class="nav-link">Tutorials</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <h2>Vue Firestore CRUD</h2>
+      <router-view />
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "app"
+}
+</script>
