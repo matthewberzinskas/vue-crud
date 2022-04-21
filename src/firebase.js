0e89c4698ed86e7f34558f7eaec91c007740f5c3
matthewberzinskas <pasomatt@gmail.com>
2022-04-21 13:19:41 -0700

    Firebase updates

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtuHR6DEr0KoS7O_is0MeVMdp64xdBLUo",
-  authDomain: "vue-crud22.firebaseapp.com",
-  projectId: "vue-crud22",
-  storageBucket: "vue-crud22.appspot.com",
-  messagingSenderId: "984840260668",
-  appId: "1:984840260668:web:925d8c6b102587e2e30739",
-  measurementId: "G-0XT1EK0K54"
+  apiKey: "AIzaSyCMLsCVKgnnAWXgqNk24jGvhLL1j-uyp-Q",
+  authDomain: "vue-crud-28eee.firebaseapp.com",
+  projectId: "vue-crud-28eee",
+  storageBucket: "vue-crud-28eee.appspot.com",
+  messagingSenderId: "534585553266",
+  appId: "1:534585553266:web:2bbed406458bd9ba79edeb",
+  measurementId: "G-MT53QGLFPB"
 };
-
 // Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
